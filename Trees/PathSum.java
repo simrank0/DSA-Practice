741b78cded6aba227d6251e935582099f6697481
Aditya-017 <adityagrand786@gmail.com>
2022-02-28 18:42:59 +0530

    Aditya Day5

---
+++ Trees/PathSum.java
@@ -0,0 +1,43 @@
+package Trees;
+
+//
+// * Definition for a binary tree node.
+//      static class TreeNode {
+//      int val;
+//      TreeNode left;
+//      TreeNode right;
+//      TreeNode() {}
+//      TreeNode(int val) { this.val = val; }
+//      TreeNode(int val, TreeNode left, TreeNode right) {
+//          this.val = val;
+//          this.left = left;
+//          this.right = right;
+//      }
+//  }
+//
+public class PathSum {
+    private static class TreeNode {
+        int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if(root==null)
+            return false;
+        if(root.left==null && root.right==null){
+            return targetSum-root.val==0;
+        }
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+
+
+
+    }
+}
