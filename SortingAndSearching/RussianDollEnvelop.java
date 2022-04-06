5b28f47c97802075f9ed110ae3e5c694aff3da65
Aditya-017 <adityagrand786@gmail.com>
2022-04-06 15:54:53 +0530

    Aditya Day11

---
+++ SortingAndSearching/RussianDollEnvelop.java
@@ -0,0 +1,30 @@
+package SortingAndSearching;
+
+import java.util.Arrays;
+
+public class RussianDollEnvelop {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+
+    public int maxEnvelopes(int[][] envelopes) {
+        // sort on increasing in first dimension and decreasing in second
+        Arrays.sort(envelopes, (a, b) -> (a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]));
+        // extract the second dimension and run LIS
+        int[] secondDim = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; ++i) secondDim[i] = envelopes[i][1];
+        return lengthOfLIS(secondDim);
+    }
+}
